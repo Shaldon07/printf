a70859a5d89450ecdba05339028a7aa755f7590b
Shaldon Maarman <shaldonm7@gmail.com>
2023-03-28 04:21:04 -0700

    0. I'm not going anywhere

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-//FLAGS
+/*FLAGS*/
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-//SIZES
+/*SIZES*/
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -42,9 +42,9 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-//FUNCTIONS
+/*FUNCTIONS*/
 
-//Functions to print chars and strings
+/*Functions to print chars and strings*/
 
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
@@ -53,7 +53,7 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-//Functions to print numbers
+/*Functions to print numbers*/
 
 int print_int(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
@@ -70,34 +70,34 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[], char buffer[], 
 	int flags, char flag_ch, int width, int precision, int size);
 
-//Function to print non printable characters
+/*Function to print non printable characters*/
 
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-//Function to print memory address
+/*Function to print memory address*/
 
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-//Functions to handle other specifiers
+/*Functions to handle other specifiers*/
 
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-//Function to print string in reverse
+/*Function to print string in reverse*/
 
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-//Function to print a string in rot 13
+/*Function to print a string in rot 13*/
 
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-//width handler
+/*width handler*/
 
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
@@ -110,7 +110,7 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size);
 
-//UTILS
+/*UTILS*/
 
 int is_printable(char);
 int append_hexa_code(char, char[], int);
